fce96ec4586968bfc68d706a76ccf90d6ae221a0
Riko Wolhuter <47495097+RikoWolhuter@users.noreply.github.com>
2026-02-13 12:02:54 +0200

    Cleaned code and added print statement

--- Intro_2/src/intro_2/Intro_2.java
+++ Intro_2/src/intro_2/Intro_2.java
@@ -1,11 +1,18 @@
 package intro_2;
-
+import java.util.Scanner;
 
 public class Intro_2 {
 
     
     public static void main(String[] args) {
+        String name;
+        
         System.out.println("hello class");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter your name:");
+        name = scan.nextLine();
+        
     }
     
 }
